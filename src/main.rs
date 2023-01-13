f21005480d34b9a7eeba22e6738bda32fe901f60
潘吉祥 <1121153008@qq.com>
2023-01-13 20:35:21 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::{OpenOptions};
+use std::io::Write;
+use std::path::PathBuf;
+use memmap2::{MmapMut};
+
 fn main() {
-    println!("Hello, world!");
+    let path: PathBuf = PathBuf::from("111111");
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&path).unwrap();
+    let mut mmap = unsafe { MmapMut::map_mut(&file).unwrap() };
+    // 从指定位置写入
+    let mut x = &mut mmap[3..];
+    x.write_all(b"abc").unwrap();
+    x.write_all(b"abc").unwrap();
+    mmap.flush().unwrap();
 }
